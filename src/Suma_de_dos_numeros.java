ff4b392def79f98cb24096abb54dfd4ccb45b44b
lety <lety@email.com>
2022-03-08 18:25:24 -0600

    Primer Commit

---
+++ src/Suma_de_dos_numeros.java
@@ -0,0 +1,9 @@
+
+public class Suma_de_dos_numeros {
+	public static void main(String[] parametro) {
+		int a = 101, b = 101, c = 0;
+	      c = a + b;
+	      System.out.println( "El resultado es: " + c );
+	}
+
+}
